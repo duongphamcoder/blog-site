e1a01b07f0bee75c86497dfc5928029f75a03e98
quy <xxboy.tlxx@gmail.com>
2022-11-03 10:53:22 +0700

    config route structure

--- blog-site/back-end/src/index.js
+++ blog-site/back-end/src/index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const route = require("./routes/index");
+route(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {
