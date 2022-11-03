bae62ba2e274961751b01a697d54b590c765e910
quy <xxboy.tlxx@gmail.com>
2022-11-03 11:33:21 +0700

    #14 config route

--- blog-site/back-end/src/index.js
+++ blog-site/back-end/src/index.js
@@ -5,10 +5,6 @@ const route = require("./routes/index");
 route(app);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, async () => {
-  try {
-    console.log("App running on port " + PORT);
-  } catch (error) {
-    console.log(error);
-  }
+app.listen(PORT, () => {
+  console.log("App running on port " + PORT);
 });
